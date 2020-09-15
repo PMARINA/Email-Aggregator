ce08008097fe9c68e4c2139bb43c1028d1a3cc39
PMARINA <PMARINA@users.noreply.github.com>
2020-09-15 17:26:05 -0400

    Updated Filepaths

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 # Future Improvements: Use telnet to verify that the email addresses are valid with the domain.
 # Why don't we do this right now?
 # It looks like many ISPs block port 25 (telnet)
+# Apparently there's a python library for this? Need to test
 
 import glob
 import os
@@ -76,36 +77,76 @@ def process_email(line: str, failed_list: list, domain_validity: dict):
             reject(line, failed_list)
 
 
-if __name__ == "__main__":
-    # Get the path of the file being run without the file at the end of the path (get containing folder)\
-    cwd = '/'.join(__file__.split("/")[:-1])
-    os.chdir(cwd)
-    work_dir = '/'.join([cwd, "PMARINA_Email_Aggregator"])
+def create_and_empty_working_directory(workdir):
     if os.path.exists(work_dir):
         shutil.rmtree(work_dir)
         os.mkdir(work_dir)
     else:
         os.mkdir(work_dir)
-    csv_files_in_cwd = glob.glob("*.csv")
-    file_counter = 1
-    for zipfilename in csv_files_in_cwd:
-        newfp = '/'.join([work_dir, str(file_counter) + ".csv"])
-        file_counter += 1
-        shutil.copyfile(zipfilename, newfp)
 
+
+def copy_all_csvs_into_working_directory(counter, wd_path):
+    csv_files_in_cwd = glob.glob("*.csv")
+    logger.debug(csv_files_in_cwd)
+    for csv_file in csv_files_in_cwd:
+        newfp = '/'.join([wd_path, str(counter) + ".csv"])
+        counter += 1
+        # Copy the csv file in and rename it to a number to prevent collisions
+        # with csv files of the same name, from zip files.
+        shutil.copyfile(csv_file, newfp)
+    return counter
+
+
+def extract_all_zips_into_working_directory(counter, wd_path):
     zip_files_in_cwd = glob.glob("*.zip")
     for zipfilename in zip_files_in_cwd:
         with zipfile.ZipFile(zipfilename, 'r') as zipobj:
-            file_name_list = zipobj.namelist()
+            file_name_list = zipobj.namelist()  # List of files in zip file
             for filename_from_zip in file_name_list:
+                # If any file in the zip (whether it's in another folder, doesn't
+                # matter), ends in csv, pull it
                 if filename_from_zip.endswith(".csv"):
-                    newfp = '/'.join([work_dir, str(file_counter) + ".csv"])
-                    file_counter += 1
+                    newfp = '/'.join([wd_path, str(counter) + ".csv"])
+                    counter += 1
+                    # This will break if there's a file of the same name (numbered csv files in the zip)
                     zipobj.extract(filename_from_zip)
                     os.rename(filename_from_zip, newfp)
+    return counter
 
-    os.chdir(work_dir)
+
+def read_csv_files_in_cwd():
     csv_files_in_work_directory = glob.glob("*.csv")
+    logger.debug(csv_files_in_work_directory)
+    combined_lines = []
+    for file in tqdm(csv_files_in_work_directory):
+        f = open(file)
+        filelines = f.readlines()
+        filelines.remove(filelines[0])  # Remove the csv header
+        combined_lines.extend(filelines)
+        f.close()
+    return combined_lines
+
+
+if __name__ == "__main__":
+    # Get the path of the file being run without the file at the end of the path (get containing folder)\
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(cwd)
+    work_dir = '/'.join([cwd, "PMARINA_Email_Aggregator"])
+
+    logger.debug("Attempting to initialize working directory: " + work_dir)
+    create_and_empty_working_directory(work_dir)
+    logger.success("Created Work Directory")
+
+    file_counter = 1
+    logger.debug("About to copy CSV files into working directory")
+    file_counter = copy_all_csvs_into_working_directory(file_counter, work_dir)
+    logger.success("All CSV files copied into working directory")
+
+    logger.debug(
+        "About to extract all zip files in WD if they contain CSV files")
+    file_counter = extract_all_zips_into_working_directory(
+        file_counter, work_dir)
+    logger.success("All zip files extracted accordingly")
 
     email_output_file = open("output.txt", 'w')
     # Create an empty set for emails, as sets don't allow duplicate items
@@ -117,13 +158,10 @@ if __name__ == "__main__":
     domain_validity = {}
     # A list of lines that failed to be processed and need a human to process them
     failed_list = []
+
+    os.chdir(work_dir)
     logger.debug("Reading in all files")
-    for file in tqdm(csv_files_in_work_directory):
-        f = open(file)
-        filelines = f.readlines()
-        filelines.remove(filelines[0])  # Remove the csv header
-        combined_lines_from_input_files.extend(filelines)
-        f.close()
+    combined_lines_from_input_files = read_csv_files_in_cwd()
     logger.success("Read in all files")
 
     logger.debug("Starting to process all ingested data")
@@ -142,6 +180,7 @@ if __name__ == "__main__":
 
     logger.debug("Starting dump from set to output.txt")
     overall_email_list = list(overall_email_set)
+    overall_email_list = sorted(overall_email_list)
     for email_string in tqdm(overall_email_list):
         email_output_file.write(email_string + "\n")
     logger.success("Wrote emails out")
