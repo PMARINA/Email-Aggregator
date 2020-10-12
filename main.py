428b834b93491667ebd465689cd0c88851cb93b5
PMARINA <bluerat33@gmail.com>
2020-10-12 01:21:05 -0400

    Improved output messages, email verification, simplified some methods
    Signed-off-by: PMARINA <bluerat33@gmail.com>

--- main.py
+++ main.py
@@ -14,17 +14,26 @@ import glob
 import os
 from loguru import logger
 from tqdm import tqdm
+
 # This is a terrible regex replacement, in future, just make your own regex \/
-from email.utils import parseaddr
 from dns.resolver import query
 import dns
 import shutil
 import zipfile
+import re
+import multiprocessing
 
-
-'''
+MAX_THREADS = 8
+NUM_THREADS = lambda num_tasks: min(MAX_THREADS, num_tasks)
+NUM_TASKS_PER_THREAD = lambda num_tasks: num_tasks / NUM_THREADS(num_tasks)
+plurality = (
+    lambda value, singular_case, plural_case, compare_to=2: singular_case
+    if value == compare_to
+    else plural_case
+)
+"""
 Given a line that has been rejected, do whatever with it (in this case, add it to a list and log it)
-'''
+"""
 
 
 def reject(line: str, failed_list: list):
@@ -32,52 +41,78 @@ def reject(line: str, failed_list: list):
     logger.error(line)
 
 
-'''
+"""
 Given a line of text, process out the email
-'''
+"""
+
+email_regex = re.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+")
+# From https://www.tutorialspoint.com/Extracting-email-addresses-using-regular-expressions-in-Python
 
 
-def process_email(line: str, failed_list: list, domain_validity: dict):
-    line = line.strip().lower()
-    if "@" not in line:
-        reject(line, failed_list)  # Emails need to have an @
+def lookup_domain(domain: str):
+    dns.resolver.resolve(domain, "MX")
+
+
+def validate_domain(domain: str, domain_validity: dict, timeout: int = 5):
+    # Check domain validity
+    assert timeout > 0
+    if domain in domain_validity:
+        if not domain_validity[domain]:
+            return False
     else:
-        returned_match = parseaddr(line)[1]
-        # Email address cannot possibly be shorter than 5 characters long
-        while "@" not in returned_match and len(line) >= 5:
-            # Sometimes the email regex matches stuff in the line before the actual email
-            # When it does so, keep going until you have an 'email' with an @ in it
-            line = line[len(returned_match)-1:]
-            returned_match = parseaddr(line)[1]
-        if "@" in returned_match:  # If the loop didn't break because of >=5 condition
-            # Sometimes the filter doesn't get rid of stuff before the start of email...
-            while "\"" in returned_match:
-                returned_match = returned_match[returned_match.index(
-                    "\"")+1:]
-                # Remove the start of the line up to and including "
-            # Get the domain from the email
-            domain = returned_match.rsplit('@', 1)[-1]
-            domain_is_valid = False
-            if domain in domain_validity:
-                domain_is_valid = domain_validity[domain]
-            else:
-                try:
-                    # Does the domain have a valid DNS lookup entry?
-                    domain_is_valid = bool(dns.resolver.resolve(domain, 'MX'))
-                except dns.exception.DNSException:
-                    logger.error(domain + " not valid")
-                    domain_is_valid = False
-                    # Don't do reject here, instead, do it in the else condition based on domain_is_valid
-                domain_validity[domain] = domain_is_valid
-            if domain_is_valid:
-                return returned_match
-            else:
-                reject(line, failed_list)
-        else:
+        try:
+            # Does the domain have a valid DNS lookup entry?
+            proc = multiprocessing.Process(
+                target=lookup_domain, args=[domain]
+            )
+            proc.start()
+            proc.join(timeout)
+            if proc.is_alive():
+                proc.kill()
+                logger.error(
+                    f"Lookup timed out for `{domain}` killing process after {timeout} seconds"
+                )
+                return False
+        except dns.exception.DNSException:
+            logger.error(domain + " not found")
+            return False
+        domain_validity[domain] = True
+    return True
+
+
+def validate_email(email_address: str, domain_validity: dict):
+    if "@" not in email_address:
+        return False
+    domain = email_address.rsplit("@", 1)[-1]
+    if not validate_domain(domain, domain_validity):
+        return False
+    return True
+
+
+def process_email(
+    line: str, failed_list: list, domain_validity: dict, valid_set: set
+):
+    line = line.strip()
+    # emails need to be at least 5 chars long ie a@b.c, if not, don't bother...
+    if len(line) < 5:
+        reject(line, failed_list)
+    else:
+        line = line.lower()
+        returned_match = email_regex.findall(line)
+        # If the list is empty or None is returned, reject.
+        if not returned_match:
             reject(line, failed_list)
+        else:
+            # User might for some reason include multiple emails in a form response...
+            for email in returned_match:
+                if validate_email(email, domain_validity):
+                    valid_set.add(email)
+                else:
+                    # This could just not exist, depending on how much manual labor you want...
+                    reject(email, failed_list)
 
 
-def create_and_empty_working_directory(workdir):
+def create_and_empty_working_directory(work_dir):
     if os.path.exists(work_dir):
         shutil.rmtree(work_dir)
         os.mkdir(work_dir)
@@ -85,38 +120,72 @@ def create_and_empty_working_directory(workdir):
         os.mkdir(work_dir)
 
 
+def get_num_lines(filepath):
+    with open(filepath) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+
 def copy_all_csvs_into_working_directory(counter, wd_path):
+    num_lines = 0
     csv_files_in_cwd = glob.glob("*.csv")
-    logger.debug(csv_files_in_cwd)
-    for csv_file in csv_files_in_cwd:
-        newfp = '/'.join([wd_path, str(counter) + ".csv"])
-        counter += 1
-        # Copy the csv file in and rename it to a number to prevent collisions
-        # with csv files of the same name, from zip files.
-        shutil.copyfile(csv_file, newfp)
-    return counter
+    if csv_files_in_cwd:
+        logger.debug("About to copy CSV files into working directory")
+        if csv_files_in_cwd:
+            logger.debug("\t".join(csv_files_in_cwd))
+            for csv_file in csv_files_in_cwd:
+                newfp = "/".join([wd_path, str(counter) + ".csv"])
+                counter += 1
+                # Copy the csv file in and rename it to a number to prevent collisions
+                # with csv files of the same name, from zip files.
+                shutil.copyfile(csv_file, newfp)
+                num_lines += get_num_lines(newfp)
+        logger.success(
+            f"{file_counter-1} CSV files copied into working directory"
+        )
+    return num_lines, counter
 
 
 def extract_all_zips_into_working_directory(counter, wd_path):
     zip_files_in_cwd = glob.glob("*.zip")
-    for zipfilename in zip_files_in_cwd:
-        with zipfile.ZipFile(zipfilename, 'r') as zipobj:
-            file_name_list = zipobj.namelist()  # List of files in zip file
-            for filename_from_zip in file_name_list:
-                # If any file in the zip (whether it's in another folder, doesn't
-                # matter), ends in csv, pull it
-                if filename_from_zip.endswith(".csv"):
-                    newfp = '/'.join([wd_path, str(counter) + ".csv"])
-                    counter += 1
-                    # This will break if there's a file of the same name (numbered csv files in the zip)
-                    zipobj.extract(filename_from_zip)
-                    os.rename(filename_from_zip, newfp)
-    return counter
+    if zip_files_in_cwd:
+        logger.debug(
+            "About to extract all zip files in WD if they contain CSV files"
+        )
+        num_lines = 0
+        for zipfilename in zip_files_in_cwd:
+            with zipfile.ZipFile(zipfilename, "r") as zipobj:
+                file_name_list = (
+                    zipobj.namelist()
+                )  # List of files in zip file
+                for filename_from_zip in file_name_list:
+                    # If any file in the zip (whether it's in another folder, doesn't
+                    # matter), ends in csv, pull it
+                    if filename_from_zip.endswith(".csv"):
+                        newfp = "/".join([wd_path, str(counter) + ".csv"])
+                        counter += 1
+                        # This will break if there's a file of the same name (numbered csv files in the zip)
+                        zipobj.extract(filename_from_zip)
+                        os.rename(filename_from_zip, newfp)
+                        num_lines += get_num_lines(newfp)
+        s = plurality(len(zip_files_in_cwd), "", "s", 1)
+        logger.success(
+            f"{len(zip_files_in_cwd)} zip file{s} examined & extracted as necessary"
+        )
+    return num_lines, counter
 
 
 def read_csv_files_in_cwd():
+    plural = plurality(file_counter, "", "s")
+    isare = plurality(file_counter, "is", "are")
+    line_plurality = plurality(num_lines, "", "s", 1)
+    logger.info(
+        f"{file_counter-1} csv file{plural} {isare} about to be processed with total {num_lines} line{line_plurality}"
+    )
     csv_files_in_work_directory = glob.glob("*.csv")
-    logger.debug(csv_files_in_work_directory)
+    files = "\t".join(csv_files_in_work_directory)
+    logger.debug(f"Reading: {files}")
     combined_lines = []
     for file in tqdm(csv_files_in_work_directory):
         f = open(file)
@@ -124,31 +193,38 @@ def read_csv_files_in_cwd():
         filelines.remove(filelines[0])  # Remove the csv header
         combined_lines.extend(filelines)
         f.close()
+    logger.success("Read in all data")
     return combined_lines
 
 
-if __name__ == "__main__":
+def pull_extract_files():
     # Get the path of the file being run without the file at the end of the path (get containing folder)\
     cwd = os.path.dirname(os.path.realpath(__file__))
     os.chdir(cwd)
-    work_dir = '/'.join([cwd, "PMARINA_Email_Aggregator"])
+    work_dir = "/".join([cwd, "PMARINA_Email_Aggregator"])
 
-    logger.debug("Attempting to initialize working directory: " + work_dir)
+    logger.debug(f"Attempting to initialize working directory: {work_dir}")
     create_and_empty_working_directory(work_dir)
-    logger.success("Created Work Directory")
+    logger.success("Work Directory Initialized")
 
     file_counter = 1
-    logger.debug("About to copy CSV files into working directory")
-    file_counter = copy_all_csvs_into_working_directory(file_counter, work_dir)
-    logger.success("All CSV files copied into working directory")
+    num_lines, file_counter = copy_all_csvs_into_working_directory(
+        file_counter, work_dir
+    )
 
-    logger.debug(
-        "About to extract all zip files in WD if they contain CSV files")
-    file_counter = extract_all_zips_into_working_directory(
-        file_counter, work_dir)
-    logger.success("All zip files extracted accordingly")
+    (
+        number_lines_zip,
+        file_counter,
+    ) = extract_all_zips_into_working_directory(file_counter, work_dir)
+    num_lines += number_lines_zip
+    os.chdir(work_dir)
+    return num_lines, file_counter
 
-    email_output_file = open("output.txt", 'w')
+
+if __name__ == "__main__":
+    num_lines, file_counter = pull_extract_files()
+    num_lines -= file_counter - 1
+
     # Create an empty set for emails, as sets don't allow duplicate items
     overall_email_set = set()
     # This is essentially a list of every line that matches the input filetype (all csv files)
@@ -158,30 +234,29 @@ if __name__ == "__main__":
     domain_validity = {}
     # A list of lines that failed to be processed and need a human to process them
     failed_list = []
-
-    os.chdir(work_dir)
-    logger.debug("Reading in all files")
     combined_lines_from_input_files = read_csv_files_in_cwd()
     logger.success("Read in all files")
 
     logger.debug("Starting to process all ingested data")
     for line in tqdm(combined_lines_from_input_files):
-        email = process_email(line, failed_list, domain_validity)
-        if email is not None:
-            overall_email_set.add(email)
+        process_email(
+            line, failed_list, domain_validity, overall_email_set
+        )
     logger.success("Processed all ingested data")
-
-    logger.debug("About to process failed lines")
-    for line in tqdm(failed_list):
-        print()
-        overall_email_set.add(
-            input("What email should we add?\n" + line + "\n").lower())
-    logger.success("Processed failed lines")
+    if failed_list:
+        logger.debug("About to process failed lines")
+        for line in tqdm(failed_list):
+            print()
+            overall_email_set.add(
+                input("What email should we add?\n" + line + "\n").lower()
+            )
+        logger.success("Processed failed lines")
 
     logger.debug("Starting dump from set to output.txt")
     overall_email_list = list(overall_email_set)
     overall_email_list = sorted(overall_email_list)
+    email_output_file = open("output.txt", "w")
     for email_string in tqdm(overall_email_list):
-        email_output_file.write(email_string + "\n")
+        email_output_file.write(f"{email_string}\n")
     logger.success("Wrote emails out")
     email_output_file.close()
